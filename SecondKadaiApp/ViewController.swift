c9cb7b31d85ac73c6a3ac2373670fe0e7931e1ba
穂坂研興 <ho0.sa1.ke0.n6.1996@docomo.ne.jp>
2016-02-23 16:58:43 +0900

    再提出

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBOutlet weak var username: UITextField!
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destinationViewController as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているnameに値を渡す
+        resultViewController.name = username.text!
+        
+        
+        
+    }
+    
+    
+    
+    
+    @IBAction func unwid (segue : UIStoryboardSegue) {
+    }
 
 }
 
